89f50709791746d47268f846c567b7d0116e9a10
zhangchi <2216336843@qq.com>
2020-12-15 10:42:04 +0800

    2020/12/15 10:41

--- src/Hegang.APP/obj/x86/Debug/Hegang.APP_Content.g.i.cs
+++ src/Hegang.APP/obj/x86/Debug/Hegang.APP_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/bitbug_favicon%20(1).ico")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/trangle.ico")]
 
 
