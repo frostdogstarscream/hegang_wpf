8cbca90019aee438828cb673c655d431ad3f0865
zhangchi <2216336843@qq.com>
2020-12-30 00:42:28 +0800

    2020/12/30 0:42

---
+++ src/Hegang.APP/ViewModels/User.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hegang.APP.ViewModels
+{
+    class User:NotificationObject
+    {
+        private string id;
+        private string userName;
+        private string pwd;
+        private string age;
+        private string nation;
+        private string department;
+        
+        public User(string _id,string _userName,string _pwd,string _age,string _nation,string _department)
+        {
+            this.Id = _id;
+            this.UserName = _userName;
+            this.Pwd = _pwd;
+            this.Age = _age;
+            this.Nation = _nation;
+            this.Department = _department;
+        }
+
+        public string Id 
+        { 
+            get => id;
+            set 
+            { 
+                id = value;
+                this.OnPropertyChanged("Id");
+            }
+        }
+        public string UserName
+        {
+            get => userName;
+            set
+            {
+                userName = value;
+                this.OnPropertyChanged("UserName");
+            }
+        }
+
+        public string Pwd
+        {
+            get => pwd;
+            set
+            {
+                pwd = value;
+                this.OnPropertyChanged("Pwd");
+            }
+        }
+
+        public string Age
+        {
+            get => age;
+            set
+            {
+                age = value;
+                this.OnPropertyChanged("Pwd");
+            }
+        }
+        public string Nation
+        {
+            get => nation;
+            set
+            {
+                nation = value;
+                this.OnPropertyChanged("Nation");
+            }
+        }
+        public string Department
+        {
+            get => department;
+            set
+            {
+                department = value;
+                this.OnPropertyChanged("Department");
+            }
+        }
+    }
+}
--- src/Hegang.APP/Views/UserManage.xaml.cs
+++ src/Hegang.APP/Views/UserManage.xaml.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using Hegang.APP.Models.DataBase;
+using Hegang.APP.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Data;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,9 +25,45 @@ namespace Hegang.APP.Views
     /// </summary>
     public partial class UserManage : Window
     {
+        private ObservableCollection<User> users;
+        private DbObject o;
         public UserManage()
         {
             InitializeComponent();
+
+            string db_str = ConfigurationManager.AppSettings["DB"];
+            o = (DbObject)Assembly.Load("Hegang.APP").CreateInstance(db_str);
+
+            usersInit();
+            this.listView.ItemsSource = users;
+        }
+
+        private void usersInit()
+        {
+            string str = "SELECT id,userName,pwd,age,nation,department FROM users;";
+            DataTable dt = o.GetDataTable(str);
+
+            users = new ObservableCollection<User>();
+            for (int i = 0; i < dt.Rows.Count; i++)
+                users.Add(new User(dt.Rows[i][0].ToString(), dt.Rows[i][1].ToString(), dt.Rows[i][2].ToString(), dt.Rows[i][3].ToString(), dt.Rows[i][4].ToString(), dt.Rows[i][5].ToString()));
+        }
+
+        private void modify_btn_Click(object sender, RoutedEventArgs e)
+        {
+            var btn = sender as Button;
+            var user = btn.DataContext as User;
+            
+        }
+
+        private void delete_btn_Click(object sender, RoutedEventArgs e)
+        {
+            var btn = sender as Button;
+            var user = btn.DataContext as User;
+
+            string str = string.Format("DELETE FROM users WHERE id = '{0}';",user.Id);
+            o.cmmdNoReturn(str);
+
+            users.Remove(user);
         }
     }
 }
